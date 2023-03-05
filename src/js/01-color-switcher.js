2ca28415e0e620bef6040a2f0c2e159cca457250
Vadim Pudovkin <113547631+vdmpdvkn@users.noreply.github.com>
2023-03-06 02:55:25 +0300

    color switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+  startBtnEl: document.querySelector('button[data-start]'),
+  stopBtnEl: document.querySelector('button[data-stop]'),
+  bodyEl: document.querySelector('body'),
+};
+const { startBtnEl, stopBtnEl, bodyEl } = refs;
+let intervalId = 0;
 
+startBtnEl.addEventListener('click', styleBgColorOnClick);
+stopBtnEl.addEventListener('click', stopStylingOnClick);
+
+function styleBgColorOnClick() {
+  startBtnEl.setAttribute('disabled', 'true');
+  stopBtnEl.removeAttribute('disabled');
+  intervalId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+function stopStylingOnClick() {
+  startBtnEl.removeAttribute('disabled');
+  stopBtnEl.setAttribute('disabled', 'true');
+  clearInterval(intervalId);
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
