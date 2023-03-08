97cad915c9cfc671c6b0edbb629fa51bd5d88083
Vadim Pudovkin <113547631+vdmpdvkn@users.noreply.github.com>
2023-03-08 03:30:02 +0300

    task 3 finished

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', submitHandler);
+function submitHandler(evt) {
+  evt.preventDefault();
+  const { delay, step, amount } = evt.currentTarget.elements;
+  let numberedDelay = Number(delay.value);
+  let numberedStep = Number(step.value);
+  const numberedAmount = Number(amount.value);
+  for (let i = 1; i <= numberedAmount; i += 1) {
+    createPromise(i, numberedDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    numberedDelay += numberedStep;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
